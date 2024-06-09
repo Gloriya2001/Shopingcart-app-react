6517d5c680770d057bd07512e032634f94d04ea6
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-09 23:35:02 +0530

    value read from Add

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Add = () => {
+
+  const [data, setData] = useState(
+    {
+      "pdtName":"",
+      "pdtCode":"",
+      "description":"",
+      "pdtimg":"",
+      "price":"",
+    }
+
+  )
+  const inputHandler = (event) => {
+    setData({...data,[event.target.name]:event.target.value})
+
+  }
+  const outputHandler = () => {
+    console.log(data)
+  }
+
+
+
   return (
     <div>
-<Navbar/>
+      <Navbar />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -16,40 +37,40 @@ const Add = () => {
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <label htmlFor="" className="form-label">Product Name</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='pdtName' value={data.pdtName} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 
                 <label htmlFor="" className="form-label">Product Code</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='pdtCode' value={data.pdtCode} onChange={inputHandler} />
 
 
               </div>
-              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
 
                 <label htmlFor="" className="form-label">Product description</label>
-                <textarea name="" id="" className="form-control"></textarea>
+                <textarea id="" className="form-control" name='description' value={data.description} onChange={inputHandler}></textarea>
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 
                 <label htmlFor="" className="form-label">Product image</label>
-                <input type="file" className="form-control" />
+                <input type="file" className="form-control" name='pdtimg' value={data.pdtimg} onChange={inputHandler}/>
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <label htmlFor="" className="form-label">Price</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='price' value={data.price} onChange={inputHandler}/>
 
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                <button className="btn btn-success">Add</button>
+                <button className="btn btn-success" onClick={outputHandler}>Add</button>
 
               </div>
 
