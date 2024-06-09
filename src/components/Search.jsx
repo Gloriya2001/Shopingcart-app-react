1b47af39a5149bdc471ee82c8cb0be5019d9579e
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-09 23:46:12 +0530

    value read from search done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Search = () => {
+
+  const [data, setData] = useState(
+
+    {
+      "pdtName": ""
+    }
+  )
+
+  const inputHandler = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value })
+  }
+  const outputHandler = () => {
+    console.log(data)
+  }
   return (
     <div>
-<Navbar/>
-            <div className="container">
-                <div className="row g-3">
-                    <h2>Search product</h2>
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+      <Navbar />
+      <div className="container">
+        <div className="row g-3">
+          <h2>Search product</h2>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-    <lable className="form-label">Product Name</lable>
-    <input type="text" className="form-control" />
+            <lable className="form-label">Product Name</lable>
+            <input type="text" className="form-control" name='pdtName' value={data.pdtName} onChange={inputHandler}/>
 
 
-                    </div>
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <button className="btn btn-success">Search</button>
-                    </div>
-                </div>
-            </div>
+          </div>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <button className="btn btn-success" onClick={outputHandler} >Search</button>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
